ab9781c656da1bb3a21f5bb322f3df877742f86c
zhaobin <kl_0810@163.com>
2025-04-09 14:19:06 +0800

    feat(calibration_utils.py, calibrate_CAM_A_OV9782.sh) suit for fisheye

--- calibrate_CAM_A_OV9782.sh
+++ calibrate_CAM_A_OV9782.sh
@@ -1,12 +1,6 @@
 # calibrate_CAM_A_OV9782.sh Description: Calibrate camera CAM_A_OV9782
 
-# squareSizeCm: 4.42cm
-# markerSizeCm: 3.30cm
-# squaresX: 13
-# squaresY: 7
-# cameraMode: perspective
-# disableCamera: left, right
-
+# Hfov40_dataset:
 python calibrate.py --squareSizeCm 4.42 \
                     --markerSizeCm 3.30 \
                     --squaresX 13 \
@@ -14,4 +8,16 @@ python calibrate.py --squareSizeCm 4.42 \
                     --cameraMode perspective \
                     --disableCamera left right \
                     --enablePolygonsDisplay \
+                    --board OAK-FFC-4P \
                     --mode process \
+
+# # Hfov180_dataset:
+# python calibrate.py --squareSizeCm 3.95 \
+#                     --markerSizeCm 2.95 \
+#                     --squaresX 13 \
+#                     --squaresY 7 \
+#                     --cameraMode Fisheye \
+#                     --disableCamera left right \
+#                     --enablePolygonsDisplay \
+#                     --board OAK-FFC-4P \
+#                     --mode process \
